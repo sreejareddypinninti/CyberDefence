d6832e3b5c92a789158a01a2da16bee8101482c8
koosuru_vardhini <koosuru_vardhini@epam.com>
2025-04-26 11:57:15 +0530

    working

--- Honeypot-Implementation-master/CaptureImage.py
+++ Honeypot-Implementation-master/CaptureImage.py
@@ -1,28 +1,3 @@
-# import cv2
-# import os
-# import time
-# import matplotlib.pyplot as plt
-#
-#
-# cap = cv2.VideoCapture(0)
-#
-# if cap.isOpened():
-#     ret, frame = cap.read()
-#     print(ret)
-#     print(frame)
-# else:
-#     ret = False
-#
-# img1 = frame
-#
-# directory = r"C:/UsersADMIN/Downloads/Honeypot/Honeypot-Implementation-master/data_stored"
-# os.chdir(directory)
-# print(os.listdir(directory))
-# filename = 'Intruder.jpg'
-# cv2.imwrite(filename, img1)
-#
-#
-# cap.release()
 import cv2
 import os
 
